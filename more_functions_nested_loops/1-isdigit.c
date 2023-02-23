f3e61cbba5373a76e2e792dcee0eb1ce83d1ffb0
xdJidx <bernardjerome59@gmail.com>
2023-02-23 05:18:56 -0800

    Write a function that checks for a digit (0 through 9).

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check is a digit
+ *@c: variable
+ *Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
