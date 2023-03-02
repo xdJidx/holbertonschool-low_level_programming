a74b5b4f80c37fcf3a9534e44b95672ce0636d09
xdJidx <bernardjerome59@gmail.com>
2023-03-02 11:57:43 -0800

    Write a function that convert a string to an integer.

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * atoi - Permet d'afficher une suite de caractère
+ * en chiffre.
+ * @s: string
+ * Return: la suite de chiffre et son signe
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+}
