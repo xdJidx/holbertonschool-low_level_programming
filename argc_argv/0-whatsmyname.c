8a20c0180fa9d5a1dafd381411a9933e668ecc0a
xdJidx <bernardjerome59@gmail.com>
2023-03-08 01:42:50 -0800

    Correction betty

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Affiche le nom du programme
+ * @argc: taille du programme
+ * @argv: chaine de caractere
+ * Return: always 0
+ */
+
 int main(int argc, char *argv[])
 {
-	while(argc--)
+	while (argc--)
 		printf("%s\n", *argv++);
-	
+
 	return (0);
 }
