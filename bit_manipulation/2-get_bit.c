d79663400641dd82c92186088fabee500c7c94d6
xdJidx <bernardjerome59@gmail.com>
2023-04-08 01:03:41 -0700

    Write a function that returns the value of a bit at a given index.-Correction Betty-

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * set_bit - retourne la valeur d'un bit à un index donné dans un nombre binaire.
+ * get_bit - retourne la valeur d'un bit à un index donné.
  * @n: Le nombre binaire dont on veut connaître la valeur d'un bit.
- * @index: index du bit que l'on souhaite la valeur, index 0 correspond au bit de poids faible.
+ * @index: index du bit que l'on souhaite la valeur.
  *
  * Return: La valeur du bit à l'index donné (0 ou 1)
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask; 
+	unsigned long int mask;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	mask = 1 << index;
 
-	return (n & mask) ? 1 : 0;
+	return ((n & mask) ? 1 : 0);
 }
