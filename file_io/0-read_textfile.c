9bc94d3caee6344f199caa4a56ccf5ea6325eb79
xdJidx <bernardjerome59@gmail.com>
2023-04-11 01:18:32 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * read_textfile - Reads texte file and prints it
+ * @filename: Le fichier à lire et afficher
+ * @letters: la taille du fichier, son nombre de character
+ *
+ * return: Le nombre de lettre lu et affiché
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t openFile, readFile, writeFile;
+	char *buffer;
+
+
+	if (!filename)
+		return (0);
+
+	openFile = open(filename, O_RDONLY);
+	if (openFile == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+		return (0);
+
+	readFile = read(openFile, buffer, letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		close(openFile);
+		return (0);
+	}
+
+	buffer[readFile] = '\0';
+	writeFile = write(STDOUT_FILENO, buffer, readFile);
+	if (writeFile == -1 || writeFile != readFile)
+	{
+		free(buffer);
+		close(openFile);
+		return (0);
+	}
+
+	free(buffer);
+	close(openFile);
+	return (writeFile);
+}
---
+++ file_io/README.md
@@ -0,0 +1,8 @@
+C - File I/O
+
+Quiz
+
+Tasks
+
+0. Tread lightly, she is near
+
